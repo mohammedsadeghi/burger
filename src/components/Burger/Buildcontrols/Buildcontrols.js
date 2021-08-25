ab326e52075c7ca3500a2fe75d820b0bda54740d
mohammad sadeghi hardangi <mohammad>
2021-08-25 21:18:21 +0430

    Creating Buildcontrol component

---
+++ src/components/Burger/Buildcontrols/Buildcontrols.js
@@ -0,0 +1,12 @@
+import { classes } from 'istanbul-lib-coverage';
+import React from 'react';
+import Classes from './Buildcontrols.module.css'
+const Buildcontrols =(props)=>{
+
+    return(
+        <div className={classes.Buildcontrols}>
+                
+        </div>
+    )
+}
+export default Buildcontrols;
