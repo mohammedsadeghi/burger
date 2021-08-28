15d250aa71c2d390be7a9ead4974f2ecec4faa2c
mohammad sadeghi hardangi <mohammad>
2021-08-29 00:41:59 +0430

    Sidedrawer has been added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react'
 import Aux from '../../hoc/Aux';
 import Toolbar from '../navigation/Toolbar/Toolbar';
 import Classes from './Layout.module.css'
+import Sidedrawer from '../navigation/Sidedrawer/Sidedrawer';
 export const Layout=(props)=>{
     return(
        <Aux>
            <Toolbar/>
+           <Sidedrawer/>
            <main className={Classes.Content}>
                {props.children}
            </main>
---
+++ src/components/navigation/Sidedrawer/Sidedrawer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import Navigationitems from '../Navigationitems/Navigationitems';
+import Classes from './Sidedrawer.module.css'
+
+const Sidedrawer =(props)=>{
+    return(
+        <div className={Classes.Sidedrawer}>
+            <Logo/>
+            <nav>
+                <Navigationitems></Navigationitems>
+            </nav>
+        </div>
+    )
+
+}
+export default Sidedrawer;
